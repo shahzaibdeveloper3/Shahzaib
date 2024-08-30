932d969a31ed81f02088a2f15c3dcf1fe145b6e7
Shahzaib khan <shahzaibdeveloper3@gmail.com>
2024-08-30 23:15:53 +0500

    first commit

---
+++ HTML/My assigment/java_card/js/data.js
@@ -0,0 +1,17 @@
+productcomputer = {
+    noofitem        :    2 ,
+    product_desc    :   'HP Cmputer i7 64 bits' ,
+    prdouct_rating  :    4 ,
+    product_price   :   'Price: 1000' ,
+    discountPricse1 :    1000 ,
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Java_script/Java_classis/Class22_JS_Opect_HTMS/js/data.js
@@ -0,0 +1,19 @@
+prdouctCompuer = {
+    noOfItems       : 3,
+    productDesc1    : 'HP Cmputer i7 64 bits',
+    productRating1  : 4,
+    productPrice1   : 15000,    
+    discountPricse1 : 1000,
+
+    productDesc2    : 'HP Cmputer i5 64 bits',
+    productRating2  : 2,
+    productPrice2   : 12000,    
+    discountPricse2 : 0,
+
+    productDesc3    : 'dfdfd  HP Cmputer i5 64 bits',
+    productRating3  : 5,
+    productPrice3   : 15000,    
+    discountPricse3 : 0,
+
+  
+}
